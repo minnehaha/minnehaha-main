bf50e169f4cf8bda86fd786e3e212b3126779532
Margots Kapacs <makapacs@hotmail.com>
2013-05-10 15:25:17 -0500

    finished front page, added blog-diary and backed up database with user extra user awinquiest

--- sites/all/themes/minnehaha/templates/page--front.tpl.php
+++ sites/all/themes/minnehaha/templates/page--front.tpl.php
@@ -127,7 +127,7 @@
                 <div class="span5 sticker">
                     <h5><i class=" icon-comment jarak-kecil"></i> Testimonial</h5>
                     <br />
-                    <div id="testimonial-list">
+                    <div id="testimonial-list" class="featured-testimonials">
                         <!-- testimonials are appended here -->
                     </div>
                     <p>
@@ -138,36 +138,46 @@
         </div>
     </div>
 </div>
-
-<hr />
-
-<div class="row">
-    <div class="span6">
-        <h1 class="subjudul">Located South Minneapolis</h1>
-        <p>
-            We provide accommodation in a beautiful part of Minneapolis near Lake Nokomis and the Missisipi River. While being near so many attractions and downtown, you still have your privacy and sense of home. Light rail service few blocks away making it convenient  to commute. A nearby bike trail makes it easy to explore this beautiful city ...
-        </p>
-        <p>
-            <a class="btn btn-small" href="#"><i class="icon-asterisk"></i> More on Location & Map</a>
-        </p>
-    </div>
-    <div class="span6">
-        <h5><i class="  icon-comment jarak-kecil"></i> Testimonial</h5>
-        <br />
-        <div id="testimonial-list">
-            <!-- testimonials are appended here -->
-        </div>
-        <p>
-            <a class="btn btn-small" href="${createLink(action: 'testimonial')}"><i class="icon-comment"></i> More</a>
-        </p>
-    </div>
-</div>
-
 <!--<div class="main-container container">-->
-    <div class="clearfix bataspanjang"></div>
+<!--    <div class="clearfix bataspanjang"></div>-->
     </div><!-- end container -->
     <div class="footers">
-        <?php print render($page['footer']); ?>
+        <div class="container">
+            <div class="row">
+                <div class="span4">
+                    <h2>Minnehaha<i>Lofts</i></h2>
+                </div>
+                <div class="span4">
+                    <h2><i class="icon-globe icon-white jarak-kecil"></i>Social Networking</h2>
+                    <p>
+                        <i class="icon-twitter icon-white"></i> <a href="https://twitter.com/minnehahalofts" target="_blank">Follow Us on Twitter</a><i></i>
+                    </p>
+                    <p>
+                        <i class="icon-fb icon-white"></i> <a href="http://www.facebook.com/minnehahalofts" target="_blank">MinnehahaLofts on Facebook</a>
+                    </p>
+                    <p>
+                        <i class="icon-gp icon-white"></i> <a href="https://plus.google.com/u/0/b/100411277821392796298/100411277821392796298/posts" target="_blank">MinnehahaLofts on Google+</a>
+                    </p>
+                </div>
+                <div class="span4">
+                    <div id="inquiry-form-small">
+                        <div class="message-section">
+                            <div class="alert alert-block">
+                                <button type="button" class="close" data-dismiss="alert">×</button>
+                            </div>
+                        </div>
+                        <h2><i class="icon-envelope icon-white jarak-kecil"></i>Inquire</h2>
+                        <p><textarea rows="3" id="inqContent" name="inqContent">Type your inquiry here.</textarea></p>
+                        <div class="input-prepend">
+                            <span class="add-on">E-Mail</span><input class="span2" id="userEmail" name="userEmail" size="16" type="text" >
+                            <a class="btn btn-success" id="submitInq" href="#"><i class="icon-ok icon-white"></i>Inquire</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <hr />
+            <footer><p>copyright <span class="minilogo">&nbsp;Minnehaha<i>Lofts</i></span> &copy; 2012. Created by : <a href="http://www.kapasoft.com">KapaSoft</a></p</footer>
+        </div>
     </div>
 </div><!-- end isi -->
     <script type="application/javascript">
