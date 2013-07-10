443d9d9c142e063c44ccfdca37d2af13707cecb1
Margots Kapacs <makapacs@hotmail.com>
2013-07-10 17:22:20 -0500

    updating for multiple views, adding seo ref link list in footer. All seo multiple views related

---
+++ sites/default/files/js/js_OD_63fum9y3rBAmTqIZ0uQn7kls4r4pBK5gf2Nd8eJc.js
@@ -0,0 +1,44 @@
+(function ($) {
+
+Drupal.behaviors.initColorbox = {
+  attach: function (context, settings) {
+    if (!$.isFunction($.colorbox)) {
+      return;
+    }
+    $('.colorbox', context)
+      .once('init-colorbox')
+      .colorbox(settings.colorbox);
+  }
+};
+
+{
+  $(document).bind('cbox_complete', function () {
+    Drupal.attachBehaviors('#cboxLoadedContent');
+  });
+}
+
+})(jQuery);
+;
+(function ($) {
+
+Drupal.behaviors.initColorboxDefaultStyle = {
+  attach: function (context, settings) {
+    $(document).bind('cbox_complete', function () {
+      // Only run if there is a title.
+      if ($('#cboxTitle:empty', context).length == false) {
+        $('#cboxLoadedContent img', context).bind('mouseover', function () {
+          $('#cboxTitle', context).slideDown();
+        });
+        $('#cboxOverlay', context).bind('mouseover', function () {
+          $('#cboxTitle', context).slideUp();
+        });
+      }
+      else {
+        $('#cboxTitle', context).hide();
+      }
+    });
+  }
+};
+
+})(jQuery);
+;
